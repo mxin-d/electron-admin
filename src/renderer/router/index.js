37eccdc9d08e86af8b99b1c6b05d243e8ce52b11
Mingxin Duan <duanmingxin@xiaomi.com>
2021-08-24 11:26:43 +0800

    feat: 屏蔽 router 异常信息

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+/**
+ * 屏蔽 router 异常信息
+ */
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject)
+  return originalPush.call(this, location).catch(err => err)
+}
+
 Vue.use(Router)
+
+
 
 /* Layout */
 import Layout from '@/layout'
